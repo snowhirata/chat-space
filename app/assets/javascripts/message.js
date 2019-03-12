e38a5f44f3d1124299137e2f127583b5206287d8
snowhirata <yukihirata@hirataakinoMacBook-puro.local>
2019-03-12 16:34:24 +0900

    Jason形式で返す

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,29 @@
 $(function(){
 
+  function buildHTML(message){
+    var insertImage = '';
+    if (message.image_url) {
+      insertImage = `<img src="${message.image_url}">`;
+    }
+    var html = `<div class = "message" data-id = "${message.id}"}>
+                  <div class = "upper-message">
+                    <div class = "upper-message__user-name">
+                      ${message.name}
+                    </div>
+                    <div class = "upper-message__date.">
+                      ${message.created_at)}
+                    </div>
+                  </div>
+                  <div class = "lower-message">
+                    <div class= "lower-message__content">
+                      ${message.body}
+                    </div>
+                      ${insertImage}
+                  </div>
+                </div>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -11,5 +35,19 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.wrapper').append(html)
+      $('#message_body').val('')
+      $('.messages').animate({scrollTop: $(".messages")[0].scrollHeight}, 1500);
+    })
+    .fail(function(){
+      alert('error');
+    })
   })
+
+  // function scroll(){
+  //   $('.message').animate({scrollTop: $('message')[0].scrollHeight}, 'fast');
+  // }
 })
