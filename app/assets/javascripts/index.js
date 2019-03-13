7f51eab29d52a365bc3b7af81819aee2c5edf635
snowhirata <yukihirata@hirataakinoMacBook-puro.local>
2019-03-13 17:43:40 +0900

    ビューの調整

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -23,7 +23,7 @@ $(function() {
     var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-8">
                   <input name="group[user_ids][]" type="hidden" value="${user_id}">
                   <p class="chat-group-user__name">${user_name}</p>
-                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                  <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
                 </div>`
     member_list.append(html);
   }
