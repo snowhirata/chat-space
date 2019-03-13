5e6bc7ccce9dc0e8bd26146337022cc5a6a6afca
snowhirata <yukihirata@hirataakinoMacBook-puro.local>
2019-03-13 11:02:00 +0900

    animateメソッド修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,34 +1,30 @@
 $(function(){
-
   function buildHTML(message){
-    var insertImage = '';
-    if (message.image_url) {
-      insertImage = `<img src="${message.image_url}">`;
-    }
-    var html = `<div class = "message" data-id = "${message.id}"}>
-                  <div class = "upper-message">
-                    <div class = "upper-message__user-name">
-                      ${message.name}
+    var message_image = message.image ? message.image : ""
+    var html = `<div class="message" data-message-id="${message.id}">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                       ${message.name}
                     </div>
-                    <div class = "upper-message__date.">
-                      ${message.created_at)}
+                    <div class="upper-message__date">
+                       ${message.created_at}
                     </div>
                   </div>
-                  <div class = "lower-message">
-                    <div class= "lower-message__content">
+                  <div class="lower-message">
+                    <p class="lower-message__content">
                       ${message.body}
-                    </div>
-                      ${insertImage}
+                    </p>
+                    <img class="lower-message__image" src="${message_image}">
                   </div>
                 </div>`
     return html;
   }
 
-  $('#new_message').on('submit', function(e){
+  $('.new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
+    var url = $(this).attr('action');
+     $.ajax({
       url: url,
       type: "POST",
       data: formData,
@@ -36,18 +32,17 @@ $(function(){
       processData: false,
       contentType: false
     })
-    .done(function(data){
+    .done(function(data) {
       var html = buildHTML(data);
-      $('.wrapper').append(html)
-      $('#message_body').val('')
-      $('.messages').animate({scrollTop: $(".messages")[0].scrollHeight}, 1500);
+      $('.messages').append(html);
+      $('.form__message, .form__mask__image').val('');
+      $('.message:last').animate({scrollTop: $('.message:last').get(0).scrollHeight}, 'slow');
     })
-    .fail(function(){
+    .fail(function() {
       alert('error');
     })
-  })
-
-  // function scroll(){
-  //   $('.message').animate({scrollTop: $('message')[0].scrollHeight}, 'fast');
-  // }
-})
+    .always(function(){
+      $('.form__submit').prop("disabled", false);
+    });
+  });
+});
